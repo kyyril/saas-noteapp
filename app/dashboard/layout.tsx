e9f37c8c54d6159799f169528b89df92d199b66d
kyyril <khairilrahman55@gmail.com>
2024-10-03 00:56:44 +0700

    feat(auth):user auth

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,19 @@
 import { DashboardNav } from "@/components/DashboardNavbar";
 import { ReactNode } from "react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { get } from "http";
+import { redirect } from "next/navigation";
 
-export default function LayoutDashboard({ children }: { children: ReactNode }) {
+export default async function LayoutDashboard({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user) {
+    return redirect("/");
+  }
   return (
     <div className="flex flex-col space-y-6 mt-10 mx-2">
       <div className="container flex flex-1 gap-12 md:grid-cols-[200px_fr]">
